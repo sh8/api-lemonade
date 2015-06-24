1b7332311420ac721f83a8d4d1f8f5b2860bcf30
shun3475 <imokenpi3475@gmail.com>
2015-06-24 22:25:15 +0900

    近くの飲食店を表示するメソッドを実装

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,8 +2,12 @@ class Restaurant < ActiveRecord::Base
   has_many :business_days
   has_many :average_prices
 
-  def self.search(lat, lon, limit=6, offset=0)
-    self.select("*", "abs(lat - #{lat}) + abs(lon - #{lon}) as dist").order("dist asc").all.offset(offset).first(limit)
+  def self.search(lat, lon)
+    self.select("*", "abs(lat - #{lat}) + abs(lon - #{lon}) as dist").order("dist asc").all.first(6)
+  end
+
+  def self.near(lat, lon, start=0, limit=20)
+    self.select("*", "abs(lat - #{lat}) + abs(lon - #{lon}) as dist").order("dist asc").all.offset(start).first(limit)
   end
 
 end
