857056f20ad403911333473bff6eaff3d7951fd0
shun3475 <imokenpi3475@gmail.com>
2015-06-24 22:17:25 +0900

    searchメソッドを実装

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,9 @@
 class Restaurant < ActiveRecord::Base
   has_many :business_days
   has_many :average_prices
+
+  def self.search(lat, lon, limit=6, offset=0)
+    self.select("*", "abs(lat - #{lat}) + abs(lon - #{lon}) as dist").order("dist asc").all.offset(offset).first(limit)
+  end
+
 end
