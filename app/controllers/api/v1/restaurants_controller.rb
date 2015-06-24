d3fe2de33f2aea7bf8d34abcfbc8122e42b084f3
shun3475 <imokenpi3475@gmail.com>
2015-06-24 22:17:11 +0900

    searchアクションを実装

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::RestaurantsController < ApplicationController
+
+  def search
+    lat = params[:lat]
+    lon = params[:lon]
+    restaurants = Restaurant.search(lat, lon)
+
+    render :json => restaurants
+  end
+
 end
