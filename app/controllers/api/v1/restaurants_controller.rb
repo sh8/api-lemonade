74659ff679842dc35e6bb65ade95d89665b98781
shun3475 <imokenpi3475@gmail.com>
2015-06-24 22:25:32 +0900

    近くの飲食店を表示するアクションを実装

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -3,7 +3,19 @@ class Api::V1::RestaurantsController < ApplicationController
   def search
     lat = params[:lat]
     lon = params[:lon]
-    restaurants = Restaurant.search(lat, lon)
+
+    restaurants = Restaurant.search(lat, lon, limit, start)
+
+    render :json => restaurants
+  end
+
+  def near
+    lat = params[:lat]
+    lon = params[:lon]
+    limit = params[:limit]
+    start = params[:start]
+
+    restaurants = Restaurant.search(lat, lon, start, limit)
 
     render :json => restaurants
   end
