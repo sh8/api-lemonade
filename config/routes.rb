ad91faf8e2d90ebe7d9715114fea70cd0b4dca65
shun3475 <imokenpi3475@gmail.com>
2015-06-25 13:02:16 +0900

    routingの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+
+      # restaurants/
       get 'restaurants/search' => 'restaurants#search'
+      get 'restaurants/near' => 'restaurants#near'
+
+      # posts/
+      post 'posts/create' => 'posts#create'
+
     end
   end
 end
