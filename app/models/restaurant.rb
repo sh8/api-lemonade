0254337e017a05f4d290cd3f2d4c79fbd4aaea3e
shun3475 <imokenpi3475@gmail.com>
2015-06-29 13:35:59 +0900

    nearメソッドの拡張

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,8 +6,12 @@ class Restaurant < ActiveRecord::Base
     self.select("*", "abs(lat - #{lat}) + abs(lon - #{lon}) as dist").order("dist asc").all.first(6)
   end
 
-  def self.near(lat, lon, start=0, limit=20)
-    self.select("*", "abs(lat - #{lat}) + abs(lon - #{lon}) as dist").order("dist asc").all.offset(start.to_i).first(limit.to_i)
+  def self.near(lat, lon, start=0, limit=20, query="")
+    if query.present?
+      self.select("*", "abs(lat - #{lat}) + abs(lon - #{lon}) as dist").order("dist asc").all.where('name like ?', "%#{query}%").offset(start.to_i).first(limit.to_i)
+    else
+      self.select("*", "abs(lat - #{lat}) + abs(lon - #{lon}) as dist").order("dist asc").all.offset(start.to_i).first(limit.to_i)
+    end
   end
-
 end
+
