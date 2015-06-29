1acbf822c3bf5a3fd1a28405880072c545144197
shun3475 <imokenpi3475@gmail.com>
2015-06-29 13:34:57 +0900

    投稿を管理するコントローラ

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,8 +1,13 @@
 class Api::V1::PostsController < ApplicationController
 
+  def index
+    @posts = Post.where('user_id = ?', params[:user_id].to_i)
+    render :json => @posts
+  end
+
   def create
     @post = Post.new
-    @post.photo_url = params[:file]
+    @post.photo = params[:file]
     @post.user_id = params[:user_id].to_i
     @post.save
     render :json => ["error"]
