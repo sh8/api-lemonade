020551916d4a2e71c5e76aa92cd5c58c17bb9389
shun3475 <imokenpi3475@gmail.com>
2015-07-03 14:48:40 +0900

    認証関連の処理を追加

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,36 @@
 class Api::V1::UsersController < ApplicationController
+
+  def sign_up
+    @user = User.new(user_attrs)
+    @user.access_token = SecureRandom.hex
+    if @user.save!
+      render :json => @user.to_json(:only => "access_token")
+    else
+      render :json => {"type" => "unauthorized", "message" => "You are not authorized."}, :status => 401
+    end
+  end
+
+  def sign_in
+    @user = User.where(:email => params[:user][:email])
+    if user.try(:authenticate, params[:user][:password])
+      @user.access_token = SecureRandom.hex
+      @user.save
+      render :json => @user.access_token
+    else
+      render :json => {"type" => "unauthorized", "message" => "You are not authorized."}, :status => 401
+    end
+  end
+
+  def sign_out
+    @user.access_token = nil
+    @user.save
+    render :json => {"type" => "logout", "message" => "logout"}
+  end
+
+  private
+  
+  def user_attrs
+    params.require(:user).permit(:screen_name, :email, :password)
+  end
+
 end
