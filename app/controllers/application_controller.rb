39eb7d266701e634a05b403f466fb39c966c26d0
shun3475 <imokenpi3475@gmail.com>
2015-07-03 14:52:52 +0900

    認証されていない場合の処理を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  private
+
+  def authenticate_user!
+    if current_user.nil?
+      render :json => {"type" => "unauthorized", "message" => "You are not authorized."}, :status => 401
+    end
+  end
+
+  def current_user
+    @current_user ||= User.where('access_token = ?', access_token).last
+  end
+
+  def access_token
+    request.headers["X-Access-Token"]
+  end
+
 end
