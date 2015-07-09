da3ce439e8eaff705947a6032c316b4486c4867c
shun3475 <imokenpi3475@gmail.com>
2015-07-09 14:09:24 +0900

    modify posts_controller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -2,14 +2,20 @@ class Api::V1::PostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @posts = Post.where('user_id = ?', params[:user_id].to_i)
-    render :json => @posts
+    @posts = Post.where('user_id = ?', current_user.id).order('id desc')
+    json = {
+      "user" => current_user,
+      "posts" => @posts.includes(:user),
+      "posts_number" => @posts.count.to_s
+    }.to_json
+    render :json => json
   end
 
   def create
     @post = Post.new
     @post.photo = params[:file]
     @post.user_id = params[:user_id].to_i
+    @post.restaurant_id = params[:restaurant_id]
     @post.save
     render :json => ["error"]
   end
