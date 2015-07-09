555fcf1763895c46e9d29243aefa9971bfb9043c
shun3475 <imokenpi3475@gmail.com>
2015-07-09 14:10:59 +0900

    add uploader

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password validations: false
   validates :password, presence: true, length: { minimum: 6 }, unless: :login_with_sns?
   has_many :user_sns_types
+  mount_uploader :profile_photo, ImageUploader
 
   def login_with_sns?
     self.uid.present?
