ad4249cbdfa8430a56ab7f91ae5b20f1b11de6a9
shun3475 <imokenpi3475@gmail.com>
2015-07-14 00:19:15 +0900

    JSONの発行方法を変更した

--- app/controllers/api/v1/time_lines_controller.rb
+++ app/controllers/api/v1/time_lines_controller.rb
@@ -2,8 +2,29 @@ class Api::V1::TimeLinesController < ApplicationController
   
   def index
     # 0の部分をcurrent_userのフレンドのPostsを取得するようにする
-    posts = Post.where('user_id = ?', 0).includes(:user, :restaurant).order('created_at desc')
-    render :json => posts.to_json(:include => {:user => {:only => [:id, :name, :profile_photo]}, :restaurant => {:only => [:id, :name, :tel, :address, :genre]}})
-  end
+    posts = Post.where('user_id = ?', 0).includes(:user, :restaurant => :master_genre).order('created_at desc')
+    json = []
+    posts.each do |p|
+      json << {
+        "restaurant_id" => p.restaurant_id,
+        "id" => p.id,
+        "restaurant" => {
+          "address" => p.restaurant.address,
+          "id" => p.restaurant.id,
+          "tel" => p.restaurant.tel,
+          "name" => p.restaurant.name,
+          "genre" => p.restaurant.genre
+        },
+        "user_id" => p.user_id,
+        "photo" => {"url" => p.photo.url},
+        "user" => {
+          "id" => p.user.id,
+          "name" => p.user.name,
+          "profile_photo" => {"url" => p.user.profile_photo.url}
+        }
+      }
+    end
 
+    render :json => json
+  end
 end
